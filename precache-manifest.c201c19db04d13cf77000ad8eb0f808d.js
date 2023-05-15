16b5d521ba6bf6d258eec376a12cbd7d7e75c8b3
Mateusz Janicki <mj4nicki@gmail.com>
2023-05-15 10:40:28 +0200

    Updates

---
+++ precache-manifest.c201c19db04d13cf77000ad8eb0f808d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6e895c2bdc2ea80b786",
+    "url": "/dishes/static/css/main.42ee437f.chunk.css"
+  },
+  {
+    "revision": "f6e895c2bdc2ea80b786",
+    "url": "/dishes/static/js/main.f6e895c2.chunk.js"
+  },
+  {
+    "revision": "04407f45332bc07b9fb0",
+    "url": "/dishes/static/js/1.04407f45.chunk.js"
+  },
+  {
+    "revision": "1753e1a60f9d20766a18",
+    "url": "/dishes/static/css/2.00ce4e05.chunk.css"
+  },
+  {
+    "revision": "1753e1a60f9d20766a18",
+    "url": "/dishes/static/js/2.1753e1a6.chunk.js"
+  },
+  {
+    "revision": "e4de37049613d487b28c",
+    "url": "/dishes/static/js/runtime~main.e4de3704.js"
+  },
+  {
+    "revision": "21a6c5f9cd2fa652835b6882009f33f8",
+    "url": "/dishes/static/media/menu.21a6c5f9.png"
+  },
+  {
+    "revision": "603a677689be5478f6d040b93f58e166",
+    "url": "/dishes/index.html"
+  }
+];
